56f1e93bb61dc666907cc70384a4ea777fda0bf0
coriocactus <69796618+coriocactus@users.noreply.github.com>
2025-04-05 10:01:45 +0100

    struc

--- serve.hs
+++ serve.hs
@@ -5,6 +5,7 @@
 import qualified Control.Monad.IO.Class as MonadIO
 import qualified Data.Text as Text
 
+import qualified Network.HTTP.Types as HTTP
 import qualified Network.Wai as Wai
 import qualified Network.Wai.Handler.Warp as Warp
 
@@ -20,34 +21,52 @@ type GreetAPI =
   "hello" :> Get '[PlainText] Text.Text :<|>
   "bye"   :> Get '[PlainText] Text.Text
 
-greetServer :: Servant.Server GreetAPI
-greetServer =
-  helloHandler :<|>
-  byeHandler
+greetServant :: Servant.Server GreetAPI
+greetServant =
+  handleHello :<|>
+  handleBye
+  where
+    handleHello :: Servant.Handler Text.Text
+    handleHello = do
+      MonadIO.liftIO $ putStrLn "Received request for /hello"
+      return "Hello from Servant!"
 
-helloHandler :: Servant.Handler Text.Text
-helloHandler = do
-  MonadIO.liftIO $ putStrLn "Received request for /hello"
-  return "Hello from Servant!"
-
-byeHandler :: Servant.Handler Text.Text
-byeHandler = do
-  MonadIO.liftIO $ putStrLn "Received request for /bye"
-  return "Goodbye from Servant!"
+    handleBye :: Servant.Handler Text.Text
+    handleBye = do
+      MonadIO.liftIO $ putStrLn "Received request for /bye"
+      return "Goodbye from Servant!"
 
 -- ===|===|===|===|===|===|===|===|===|===|===|===|===|===|===|===|===|===|===|
 -- | +[API(+[ENDPOINT]) <-> SERVER(+[HANDLER])] + PROXY -> APPLICATION
 
 type API = GreetAPI :<|> Servant.EmptyAPI
 
-server :: Servant.Server API
-server = greetServer :<|> Servant.emptyServer
+servant :: Servant.Application
+servant = Servant.serve translator servants
+  where
+    translator :: Servant.Proxy API
+    translator = Servant.Proxy
 
-proxy :: Servant.Proxy API
-proxy = Servant.Proxy
+    servants :: Servant.Server API
+    servants = greetServant :<|> Servant.emptyServer
 
-app :: Servant.Application
-app = Servant.serve proxy server
+-- ===|===|===|===|===|===|===|===|===|===|===|===|===|===|===|===|===|===|===|
+-- | MIDDLEWARE
+
+notFoundMiddleware :: Wai.Middleware
+notFoundMiddleware app req respond = app req $ \response ->
+  case Wai.responseStatus response of
+    status
+      | status == HTTP.status404 -> do
+          MonadIO.liftIO $ putStrLn $ "Not Found: " ++ show (Wai.rawPathInfo req)
+          respond $ Wai.responseLBS
+            HTTP.status404
+            [("Content-Type", "text/plain")]
+            "Not Found: The requested resource does not exist."
+    _ -> respond response
+
+application :: Wai.Application
+application = notFoundMiddleware servant
 
 -- ===|===|===|===|===|===|===|===|===|===|===|===|===|===|===|===|===|===|===|
 -- | MAIN
@@ -57,4 +76,4 @@ main = do
   let port = 8080
   putStrLn $ "Starting server on http://localhost:" ++ show port ++ "/hello"
   putStrLn $ "Starting server on http://localhost:" ++ show port ++ "/bye"
-  Warp.run port app
+  Warp.run port application
